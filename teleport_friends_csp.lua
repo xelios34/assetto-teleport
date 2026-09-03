d667b5a6289bfeb2bef2f37ad9f58a5bf94fe906
xelios34 <xelioselita@gmail.com>
2026-09-03 15:01:49 +0300

    Update teleport_friends_csp.lua

--- teleport_friends_csp.lua
+++ teleport_friends_csp.lua
@@ -258,7 +258,7 @@ end
 -- Chat benzeri arka plan saydamlığı
 local windowBackgroundAlpha = ui.SmoothInterpolation(1.0, 2.0)
 local windowContentAlpha = ui.SmoothInterpolation(1.0, 2.0)
-local ACTIVE_BACKGROUND_ALPHA = 0.60
+local ACTIVE_BACKGROUND_ALPHA = 0.20
 local INACTIVE_BACKGROUND_ALPHA = 0.0
 
 local teleportApp = ui.addSettings({
